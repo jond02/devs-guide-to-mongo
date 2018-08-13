036886ff35047e4c152e174c403558cdd81ce001
Jonathan Dann <jdann@rei.com>
2018-08-13 14:42:18 -0700

    skip, limit, and sort

--- test/middleware_test.js
+++ test/middleware_test.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const assert = require("assert");
 const User = require("../src/user");
 const BlogPost = require("../src/blog_post");
--- test/reading_test.js
+++ test/reading_test.js
@@ -4,11 +4,15 @@ const User = require("../src/user");
 
 describe("Reading records", () => {
 
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach(done => {
        joe = new User({ name: "Joe"});
-       joe.save().then(() => done());
+       maria = new User({ name: "Maria"});
+       alex = new User({ name: "Alex"});
+       zach = new User({ name: "Zach"});
+       Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
+           .then(() => done());
     });
 
     it("finds all users with name of joe", done => {
@@ -23,6 +27,19 @@ describe("Reading records", () => {
         User.findOne({ _id: joe._id })
             .then(user => {
                 assert(user.name === "Joe");
+                done();
+            });
+    });
+
+    it('can skip and limit result set', done => {
+        User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2)
+            .then(users => {
+                assert(users.length === 2);
+                assert(users[0].name === "Joe");
+                assert(users[1].name === "Maria");
                 done();
             });
     });
